9780383ae47477c22dfd59911a0e0b368d8b9b11
Jonathan Chancey <jonathan@chancey.dev>
2024-03-05 16:55:21 -0800

    add second instance

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@
 resource "google_compute_instance" "prom-alpha" {
   boot_disk {
     auto_delete = true
-    device_name = "turn-firefly-20240306-000901"
+    device_name = "prom-alpha"
 
     initialize_params {
       image = "projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20240228"
@@ -14,10 +14,6 @@ resource "google_compute_instance" "prom-alpha" {
 
     mode = "READ_WRITE"
   }
-
-  can_ip_forward      = false
-  deletion_protection = false
-  enable_display      = false
 
   labels = {
     goog-ec-src = "vm_add-tf"
@@ -53,10 +49,55 @@ resource "google_compute_instance" "prom-alpha" {
     scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
   }
 
-  shielded_instance_config {
-    enable_integrity_monitoring = true
-    enable_secure_boot          = false
-    enable_vtpm                 = true
+  zone = "us-central1-a"
+}
+
+resource "google_compute_instance" "prom-beta" {
+  boot_disk {
+    auto_delete = true
+    device_name = "prom-beta"
+
+    initialize_params {
+      image = "projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20240228"
+      size  = 10
+      type  = "pd-standard"
+    }
+
+    mode = "READ_WRITE"
+  }
+
+  labels = {
+    goog-ec-src = "vm_add-tf"
+  }
+
+  machine_type = "e2-micro"
+
+  metadata = {
+    ssh-keys = var.ssh_keys
+  }
+
+  name = "prom-beta"
+
+  network_interface {
+    access_config {
+      network_tier = "STANDARD"
+    }
+
+    queue_count = 0
+    stack_type  = "IPV4_ONLY"
+    subnetwork  = "projects/turn-firefly/regions/us-central1/subnetworks/default"
+  }
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+    preemptible         = false
+    provisioning_model  = "STANDARD"
+  }
+
+  service_account {
+    email  = var.service_account_email
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
   }
 
   zone = "us-central1-a"
